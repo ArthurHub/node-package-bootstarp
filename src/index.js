da5b76fb481c9d993cabafa0b6a05248e9e0647f
ArthurHub <5463635+ArthurHub@users.noreply.github.com>
2024-12-05 12:15:57 -0800

    update to use @yao-pkg/pkg

--- src/index.js
+++ src/index.js
@@ -22,19 +22,22 @@ import { getNodeExecutable } from './node-exec-handler.js';
 import { getNodeModules } from './node-deps-handler.js';
 import { archiveAssets } from './assets-handler.js';
 
-const PACK_FOLDER = '.node-pkg-exec';
+const PACK_FOLDER = '.node-pkg-exec-cache';
 
 async function main() {
   const program = new Command();
   program
     .name('pkg-bootstrap')
-    .description('CLI to package a node application into a single executable')
+    .description(
+      'CLI to package a node application into a single executable to run on any ' +
+        'device without pre-requisites, dependencies, or environment compatibility issues.',
+    )
     .version('0.1.0');
   program
-    .arguments('<name>', 'Name of the package')
     .arguments('<app-path>', 'Path to the node application to package')
+    .option('-n, --name <name>', 'The name of the executable', './')
     .option('-o, --output <output>', 'Output directory for the package', './')
-    .option('-t, --target <target>', 'Target platform for the package', 'latest-win-x64')
+    .option('-t, --target <target>', 'Target platform for the package', 'node20-win-x64')
     .option('-d, --dependencies [dependencies...]', 'Dependencies to include in the package')
     .action((name, appPath, options) => {
       run(name, appPath, options.output, options.target, options.dependencies);
